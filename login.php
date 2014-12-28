a277c160a474e4a0d635b175b87362be32d8f4eb
miko0706 <mikolovecnblue@gmail.com>
2014-12-28 15:48:18 +0800

    1221
    test

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+
+帳號：<input type="text" name="id"><br>
+密碼：<input type="text" name="pass"><br>
+再次確認密碼:<input type="text" name="pass2"><br>
+姓名：<input type="text" name="name"><br>
+電話：<input type="text" name="phone"><br>
+信箱：<input type="text" name="email"><br>
+
+<input type="submit" value="確定">
+<input type="submit" value="清除">
+
+</html>
